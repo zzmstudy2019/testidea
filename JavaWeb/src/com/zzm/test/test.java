82b881e346971b1300bd3bbbc8068fd6e92a252b
1203310034 <1203310034@qq.com>
2020-09-20 15:59:06 +0800

    代码提交

---
+++ JavaWeb/src/com/zzm/test/test.java
@@ -0,0 +1,12 @@
+package com.zzm.test;
+
+/**
+ * @author zzm
+ * @date 2020-09-20 15:56
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1221");
+    }
+
+}
