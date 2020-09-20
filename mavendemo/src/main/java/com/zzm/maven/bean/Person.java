636d4756de2264381437ea02fb2099aa89874115
1203310034 <1203310034@qq.com>
2020-09-20 19:53:59 +0800

    代码提交3

---
+++ mavendemo/src/main/java/com/zzm/maven/bean/Person.java
@@ -0,0 +1,18 @@
+package com.zzm.maven.bean;
+
+/**
+ * @author zzm
+ * @date 2020-09-20 19:50
+ */
+public class Person {
+
+    private String name;
+
+    private String age;
+
+    private String sex;
+
+
+
+
+}
